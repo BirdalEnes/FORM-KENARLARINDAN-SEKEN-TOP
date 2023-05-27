e705d7414b25be8d3dfae10eeec4187d2cd71394
Enes Birdal ÇAVUŞ <Enes Birdal AVU@BUKET>
2023-05-27 22:20:52 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace FORM_KENARLARINDAN_SEKEN_TOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.kare = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // kare
+            // 
+            this.kare.BackColor = System.Drawing.Color.Aqua;
+            this.kare.Enabled = false;
+            this.kare.FlatAppearance.BorderSize = 0;
+            this.kare.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kare.Location = new System.Drawing.Point(348, 174);
+            this.kare.Name = "kare";
+            this.kare.Size = new System.Drawing.Size(80, 80);
+            this.kare.TabIndex = 0;
+            this.kare.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kare);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button kare;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FORM_KENARLARINDAN_SEKEN_TOP
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dikeyYon = 1, yatayYon = 1;
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if ((kare.Bottom >= this.Height) || kare.Top<=0)
+                dikeyYon *= -1;
+
+            if ((kare.Right >= this.Width) || kare.Left<=0)
+                yatayYon *= -1;
+
+            kare.Top+=dikeyYon;
+            kare.Left+=yatayYon;
+        }
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
